d259cc778cb3e7887899e40555a1fc4beadecfc3
Onur Altuğ <onur7altug@gmail.com>
2024-03-13 17:22:37 +0300

    commit new main

---
+++ src/main/java/com/patika/kredinbizdenservice/Main.java
@@ -0,0 +1,10 @@
+package com.patika.kredinbizdenservice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Deneme");
+		System.out.println("Onur Altuğ");
+	}
+
+}
