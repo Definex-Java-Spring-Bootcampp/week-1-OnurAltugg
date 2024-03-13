9c91c5bccf0ac2b1321c7f74f997082e57dc0931
Onur Altuğ <onur7altug@gmail.com>
2024-03-13 19:15:41 +0300

    create UserRepository class

---
+++ src/main/java/com/patika/kredinbizdenservice/model/UserRepository.java
@@ -0,0 +1,19 @@
+package com.patika.kredinbizdenservice.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+	
+	private static final List<User> users = new ArrayList<>();
+	
+	public static boolean isEmailAlreadyExists(String email) {
+        return users.stream().anyMatch(user -> user.getEmail().equals(email));
+    }
+	
+	public static void saveUser(User user) {
+        if (!isEmailAlreadyExists(user.getEmail())) {
+            users.add(user);
+        } 
+    }
+}
